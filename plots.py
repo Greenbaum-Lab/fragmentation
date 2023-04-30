69b1c4f97a01edd09739435609926beb84d70aeb
ohad-peled <121860687+ohad-peled@users.noreply.github.com>
2023-04-30 15:38:10 +0300

    update

--- plots.py
+++ plots.py
@@ -3,18 +3,22 @@ from statistics import mean
 import networkx as nx
 import matplotlib.pyplot as plt
 from joypy import joyplot
-import seaborn as sns
+
+import Transformation
 from fragmentation import remove_edge_random
 from fragmentation import remove_edge_correlated
-from fragmentation import remove_edges_distance
+from fragmentation import remove_edge_distance
 from funcs import make_fst_list
 from funcs import make_fst_dist
 from funcs import make_fst_stat
 from funcs import make_het_list
 from funcs import make_het_dist
 from funcs import make_het_stat
-
-n = 5  # no. of nodes
+from funcs import frag_random
+from funcs import frag_cor
+from funcs import frag_dist
+from funcs import calculate_centrality
+n = 10  # no. of nodes
 p = 0.8  # probability to connect nodes
 seed = 666
 net = nx.erdos_renyi_graph(n=n, p=p)  # create network
@@ -22,75 +26,56 @@ n_frag = 5000  # no. of fragmentation steps
 pos = nx.spring_layout(net, seed=55)  # set the fixed position for plotting the network
 # random.seed(12)  # set random seed
 
+central_rand = calculate_centrality(remove_edge_random(net, n_frag))
+central_cor = calculate_centrality(remove_edge_correlated(net, n_frag))
+central_dist = calculate_centrality(remove_edge_distance(net, n_frag))
 
-# analyze random fragmantation
-# def frag_random(net, n_frag:int):
-#     xxx = remove_edge_random(migration=net, n=n_frag)
-#     fst = make_fst_list(migration_list=xxx)
-#     fst_dens = make_fst_dist(fst)
-#     fst_stat = make_fst_stat(fst_dens)
-#     return fst_stat
-
-
-# rand = frag_random(net=net, n_frag=10)
-rand = remove_edge_random(migration=net, n=50)
+rand = frag_random(net=net, n_frag=n_frag)
+cor = frag_cor(net=net, n_frag=n_frag)
+dist = frag_dist(net=net, n_frag=n_frag)
 
 
-fst = make_fst_list(migration_list=rand)
-print(len(rand))
-
-
-
-
-# def frag_cor(net, n_frag):
-#     xxx = remove_edge_correlated(migration=net, n=n_frag)
-#     fst = make_fst_list(migration_list=xxx)
-#     fst_dens = make_fst_dist(fst)
-#     fst_stat = make_fst_stat(fst_dens)
-#     return fst_stat
-#
-# frag_cor(net=net, n_frag=n_frag)
-
-# # #analyze correlated fragmantation
-# migration_list_correlated = remove_edge_correlated(migration=net2, n=50)
-# fst_correlated = make_fst_list(migration_list=migration_list_correlated)
-# fst_dens_correlated = make_fst_dist(fst_correlated)
-# fst_stat_cor = make_fst_stat(fst_dens_correlated)
-#
-# #analyze distance dependent fragmantation
-# migration_list_distance = remove_edges_distance(migration=net3, n=40)
-# fst_distance = make_fst_list(migration_list=migration_list_distance)
-# fst_dens_distance = make_fst_dist(fst_distance)
-# fst_stat_dist = make_fst_stat(fst_dens_distance)
-#
-
+# het = make_het_list(rand)
+# het_dist = make_het_dist(het)
+# het_stat = make_het_stat(het_dist)
+# print(het_stat)
 
 # plotting avg and median
-# plt.plot(fst_stat_rand['step'], fst_stat_rand['avg'], label="average", color="blue")
-# plt.plot(fst_stat_rand['step'], fst_stat_rand['median'], label="median", color="blue", linestyle='dashed')
-# plt.plot(fst_stat_cor['step'], fst_stat_cor['avg'], label="average", color="red")
-# plt.plot(fst_stat_cor['step'], fst_stat_cor['median'], label="median", color="red", linestyle='dashed')
-# plt.plot(fst_stat_dist['step'], fst_stat_dist['avg'], label="average", color="green")
-# plt.plot(fst_stat_dist['step'], fst_stat_dist['median'], label="median", color="green", linestyle='dashed')
+plt.plot(rand['step'], rand['avg'], label="average", color="blue")
+plt.plot(rand['step'], rand['median'], label="median", color="blue", linestyle='dashed')
+plt.plot(cor['step'], cor['avg'], label="average", color="red")
+plt.plot(cor['step'], cor['median'], label="median", color="red", linestyle='dashed')
+plt.plot(dist['step'], dist['avg'], label="average", color="green")
+plt.plot(dist['step'], dist['median'], label="median", color="green", linestyle='dashed')
 
-# plt.xlabel('fragmentation process')
-# plt.ylabel('pairwise fst')
-# plt.legend()
-# plt.show()
-#
-#
+plt.xlabel('fragmentation process')
+plt.ylabel('pairwise fst')
+plt.legend()
+plt.show()
 
+# plotting avg and median
+plt.plot(central_rand['clustering'], rand['avg'], label="average", color="blue")
+plt.plot(central_cor['clustering'], cor['avg'], label="average", color="red")
+plt.plot(central_dist['clustering'], dist['avg'], label="average", color="green")
+
+plt.xlabel('fragmentation process')
+plt.ylabel('pairwise fst')
+plt.legend()
+plt.show()
+
+
+#
 # # # # plot distribution of Fst values - ridge-lines
 # plt.figure()
 # joyplot(
-#     data=fst_dens[['fst', 'step']],
+#     data=het_dist[['het', 'step']],
 #     by='step',
 #     colormap=plt.cm.autumn, fade=True,
 #     figsize=(12, 8)
 # )
 # plt.title('pairwise Fst along fragmentation', fontsize=16)
 # plt.show()
-#
+
 
 
 # het = make_het_list(migration_list)
@@ -99,8 +84,8 @@ print(len(rand))
 
 
 #
-#
-# # plotting avg and median
+
+# plotting avg and median
 # plt.plot(het_stat['step'], het_stat['avg'], label="average")
 # plt.plot(het_stat['step'], het_stat['median'], label="median")
 #
@@ -108,4 +93,4 @@ print(len(rand))
 # plt.ylabel('unscaled heterozygosity')
 # plt.legend()
 # plt.show()
-#
+
