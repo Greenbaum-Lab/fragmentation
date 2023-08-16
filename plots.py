059007411f203d08d68458a4cb8a30900fcba0c4
ohad-peled <121860687+ohad-peled@users.noreply.github.com>
2023-08-16 09:26:47 +0300

    update

--- plots.py
+++ plots.py
@@ -151,8 +151,8 @@ from funcs3 import make_replicates, make_replicates_new, make_networks
 
 
 print("here i start!")
-n = 20  # no. of nodes
-n_rep = 20
+n = 50  # no. of nodes
+n_rep = 100
 net = "RGG"
 ignore = False
 
