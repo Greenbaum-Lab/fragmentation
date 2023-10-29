00bd65af600afb943483bd72092b46f59dc7a86b
ohad-peled <121860687+ohad-peled@users.noreply.github.com>
2023-10-29 12:59:32 +0200

    update

--- nodes.py
+++ nodes.py
@@ -6,6 +6,9 @@ import pandas as pd
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from community import community_louvain
+
+from network_analysis import plot_network_realization
 
 
 def add_replica(df, step_column='step'):
@@ -13,13 +16,12 @@ def add_replica(df, step_column='step'):
     add a column with the number of replica for each row based on 'step'
     """
     # Find where the step resets
-    df['replica'] = (df[step_column] < df[step_column].shift(1)).astype(int)
+    df.loc[:, 'replica'] = (df[step_column] < df[step_column].shift(1)).astype(int)
 
     # Compute the cumulative sum to get the replica number
-    df['replica'] = df['replica'].cumsum()
+    df.loc[:, 'replica'] = df['replica'].cumsum()
 
     return df
-
 
 def extract_nodes(df):
     """
@@ -35,30 +37,32 @@ def extract_nodes(df):
     random_index = np.random.randint(0, nodes)
 
     # Select the rows corresponding to the randomly selected index for each step
-    selected_rows = df.iloc[100::nodes]
+    selected_rows = df.iloc[100::nodes].copy()
 
     return selected_rows
 
 
-with open('RGG, rand_ignore_False.pickle', 'rb') as file:
+
+with open('RGG, dist_ignore_False.pickle', 'rb') as file:
     rand = pickle.load(file)
+
 
 het = rand[2]
 het.reset_index(drop=True, inplace=True)
-print(het)
 het_values = extract_nodes(het)
 het_values = add_replica(het_values)
-print(het_values)
 
-# plot all lines\populations together
-# Creating a pivot DataFrame to organize the data by replica
-pivot_df = het_values.pivot(columns='replica',index='step', values='het')
-print(pivot_df)
-plt.plot(het_values['step'].unique(), pivot_df, color='lightgrey',alpha=0.5)
+# Create a pivot DataFrame
+pivot_df = het_values.pivot(columns='replica', index='step', values='het')
+
+# Plot using the pivot DataFrame
+plt.figure(figsize=(10, 6))
+plt.plot(pivot_df.index, pivot_df, color='grey', alpha=0.5)
 
 plt.xlabel('Step')
 plt.ylabel('Heterozygosity')
-plt.title('Plot of Het against Step')
+plt.title('Plot of Het against Step-distance')
+plt.grid(True)
 plt.show()
 
 
