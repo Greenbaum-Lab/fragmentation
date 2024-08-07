2f18ea725b20f721d450acfb8af0342295355e25
ohad-peled <121860687+ohad-peled@users.noreply.github.com>
2024-08-07 10:17:36 +0300

    update

--- processes.py
+++ processes.py
@@ -346,7 +346,7 @@ def remove_edge_optimal(net: nx.Graph) -> list:
     migration = net.copy()
     migration_list = [migration.copy()]  # start with the original network
 
-    while nx.number_of_edges(migration) > 2:  # Adjust the condition as needed
+    while nx.number_of_edges(migration):  # Adjust the condition as needed
         # Compute edge betweenness centrality to determine the importance of edges
         centrality = nx.edge_betweenness_centrality(migration)
 
