23bb6a40dda25448cdc480d11d7df37d9bc40656
ohad-peled <ohad.peled@mail.huji.ac.il>
2025-06-10 10:07:10 +0300

    refactor distance matrices calculations

--- network_analysis.py
+++ network_analysis.py
@@ -1,409 +1,16 @@
 
 
-################################
-################################ stack plot
-def measure_isolated_nodes(network: nx.Graph) -> int:
-    """
-    Measure the number of isolated nodes in the network.
-    :param network: NetworkX graph
-    :return: Number of isolated nodes
-    """
-    isolated_nodes = list(nx.isolates(network))
-    return len(isolated_nodes) / len(network)
 
 
-def measure_components(network: nx.Graph, min_size: int = 4) -> int:
-    """
-    Measure the number of components with a size greater than or equal to a given threshold,
-    excluding the giant component.
-    :param network: NetworkX graph
-    :param min_size: Minimum size of components to be counted
-    :return: Number of nodes in large components excluding the giant component
-    """
-    largest_component = max(nx.connected_components(network), key=len)
 
-    components = [
-        comp for comp in nx.connected_components(network)
-        if (comp != largest_component or len(comp) == min_size) and len(comp) >= min_size
-    ]
 
-    return sum(len(comp) for comp in components) / len(network)
 
 
-def measure_waste(network: nx.Graph, max_size: int = 3, min_size: int = 2) -> int:
-    """
-    Measure the number of components with a size greater than or equal to a given threshold,
-    excluding the giant component.
-    :param network: NetworkX graph
-    :param min_size: Minimum size of components to be counted
-    :return: Number of nodes in large components excluding the giant component
-    """
-    components = [comp for comp in nx.connected_components(network) if min_size <= len(comp) <= max_size]
-    num_nodes_in_medium_components = sum(len(comp) for comp in components)
-    return num_nodes_in_medium_components / len(network)
 
 
-def measure_network_metrics(networks: list) -> pd.DataFrame:
-    """
-    Measure various metrics of the networks and return them as a DataFrame:
-    - Size of the giant component
-    - Number of isolated nodes
-    - Number of components with 4 or more nodes excluding the giant component
-    :param networks: List of NetworkX graphs
-    :return: DataFrame with metrics for each network
-    """
-    metrics = []
 
-    for step, network in enumerate(networks):
-        giant_component = measure_giant_component(network)
-        isolated_nodes = measure_isolated_nodes(network)
-        components = measure_components(network)
-        waste = measure_waste(network)
 
-        total = giant_component + isolated_nodes + components + waste
 
-        # Round the first three metrics
-        giant = round(giant_component / total, 2)
-        isolated = round(isolated_nodes / total, 2)
-        components = round(components / total, 2)
-
-        # Adjust the last metric so the total sums up to 1
-        waste = 1 - giant - isolated - components
-
-        scaled_metrics = {
-            "step": step,
-            "giant": giant,
-            "isolated": isolated,
-            "components": components,
-            "waste": waste,
-        }
-
-        metrics.append(scaled_metrics)
-
-    return pd.DataFrame(metrics)
-
-
-def measure_network_metrics_replicas(replicas: list) -> pd.DataFrame:
-    """
-    Measure metrics for a list of lists of networks (replicas) and return a DataFrame
-    including a column for the replica index.
-    :param replicas: List of lists of NetworkX graphs
-    :return: DataFrame with metrics for each network and replica
-    """
-    all_metrics = []
-
-    for replica_index, networks in enumerate(replicas):
-        replica_metrics = measure_network_metrics(networks)
-        replica_metrics['replica'] = replica_index
-        all_metrics.append(replica_metrics)
-
-    return pd.concat(all_metrics, ignore_index=True)
-
-
-def calculate_statistics(df):
-    """Calculate mean and 95% confidence interval for all columns in the dataframe."""
-    result = []
-
-    # Select all columns except 'step' and 'replica'
-    columns_to_analyze = df.columns.difference(['step', 'replica'])
-
-    for column in columns_to_analyze:
-        mean_values = round(df.groupby('step')[column].mean(), 3)
-
-        # Create a DataFrame for this column's statistics
-        column_stats = pd.DataFrame({
-            'step': mean_values.index,
-            f'{column}': mean_values.values,
-            # f'{column}_ci': confidence_interval.values
-        })
-
-        result.append(column_stats)
-
-    # Concatenate all column statistics DataFrames along the 'step' index
-    result_df = pd.concat(result, axis=1)
-
-    # Remove duplicate 'step' columns
-    result_df = result_df.loc[:, ~result_df.columns.duplicated()]
-    # result_df['waste'] = 1 - result_df['giant'] - result_df['isolated'] - result_df['components']
-
-    return result_df
-
-
-def plot_network_stacked_area(df: pd.DataFrame, frag: str):
-    """
-    Plot the metrics as stacked area charts.
-    :param df: DataFrame containing the metrics to plot
-    :param frag: Fragmentation type
-    """
-
-    # Ensure the DataFrame is sorted by 'step'
-    df = df.sort_values(by='step')
-    df = normalize_steps(df)
-
-    # Create a new figure and axes with a specific size
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    # Define the columns to plot and the colors to use
-    columns = ['waste', 'isolated', 'components', 'giant']
-    colors = plt.cm.Dark2.colors[:len(columns)]
-
-    # Prepare the data for the stackplot
-    x_values = df['step'].values
-    y_values = [df[col].values for col in columns]
-
-    # Create the stackplot
-    ax.stackplot(x_values, y_values, labels=columns, colors=colors, alpha=0.8)
-
-    # Set parameters
-    ax.set_xlabel('% fragmentation', fontsize=20)
-    ax.set_ylabel('Proportion', fontsize=22)
-    ax.tick_params(axis='both', which='major', labelsize=18)  # Increase the size of the tick labels
-    ax.set_title(frag)
-    plt.ylim(0, 1)
-    # plt.legend(loc='upper left')
-
-    plt.savefig(f'./figs/stack_{frag}.jpg')
-    plt.show()
-
-
-
-def plot_network_stacked_area_all(data: dict):
-    """
-    Plot the metrics as stacked area charts for all fragmentation types in a single figure with two columns.
-    :param data: Dictionary containing the metrics to plot for each fragmentation type
-    """
-
-    fragmentation_types = list(data.keys())
-    num_plots = len(fragmentation_types)
-    num_cols = 2
-    num_rows = (num_plots + num_cols - 1) // num_cols
-
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, num_rows * 5))
-    axes = axes.flatten()
-
-    for i, frag_type in enumerate(fragmentation_types):
-        df = data[frag_type]
-        df = df.sort_values(by='step')
-        df = normalize_steps(df)
-
-        ax = axes[i]
-        columns = ['waste', 'isolated', 'components', 'giant']
-        colors = plt.cm.Dark2.colors[:len(columns)]
-
-        x_values = df['step'].values
-        y_values = [df[col].values for col in columns]
-
-        ax.stackplot(x_values, y_values, labels=columns, colors=colors, alpha=0.8)
-        ax.set_xlabel('% fragmentation', fontsize=20)
-        ax.set_ylabel('Proportion', fontsize=22)
-        ax.tick_params(axis='both', which='major', labelsize=18)
-        ax.set_title(frag_type)
-        ax.set_ylim(0, 1)
-
-    for j in range(i + 1, len(axes)):
-        fig.delaxes(axes[j])
-
-    plt.tight_layout()
-    plt.savefig('./figs/stack_all_fragmentation_types.svg')
-    plt.show()
-
-
-
-
-def plot_network_stacked_area_all(data: dict):
-    """
-    Plot the metrics as stacked area charts for all fragmentation types in a single figure with two columns.
-    :param data: Dictionary containing the metrics to plot for each fragmentation type
-    """
-
-    fragmentation_types = list(data.keys())
-    num_plots = len(fragmentation_types)
-    num_cols = 2
-    num_rows = (num_plots + num_cols - 1) // num_cols
-
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, num_rows * 5))
-    axes = axes.flatten()
-
-    for i, frag_type in enumerate(fragmentation_types):
-        networks = data[frag_type][1]
-        matrices = measure_network_metrics_replicas(networks)
-        stats = calculate_statistics(matrices)
-        stats = normalize_steps(stats)
-        df = stats.sort_values(by='step')
-        df = normalize_steps(df)
-
-        ax = axes[i]
-        columns = ['waste', 'isolated', 'components', 'giant']
-        colors = plt.cm.Dark2.colors[:len(columns)]
-
-        x_values = df['step'].values
-        y_values = [df[col].values for col in columns]
-
-        ax.stackplot(x_values, y_values, labels=columns, colors=colors, alpha=0.8)
-        ax.set_xlabel('% fragmentation', fontsize=20)
-        ax.set_ylabel('Proportion', fontsize=22)
-        ax.tick_params(axis='both', which='major', labelsize=18)
-        ax.set_title(frag_type)
-        ax.set_ylim(0, 1)
-
-    for j in range(i + 1, len(axes)):
-        fig.delaxes(axes[j])
-
-    plt.tight_layout()
-    plt.savefig('./figs/stack_all_fragmentation_types.svg')
-    plt.show()
-
-
-
-##############################plot stacks
-
-# fragmentation_types = ['rand', 'cor', 'intr', 'dist', 'reg', 'div', 'opt', 'wrst']
-# data = load_data(fragmentation_types)
-# networks = data[fragmentation_types[0]][1]
-# matrices = measure_network_metrics_replicas(networks)
-#
-# stats = calculate_statistics(matrices)
-# # plot_network_stacked_area(stats,frag=fragmentation_types[0])
-# plot_network_stacked_area_all(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##fst-distance relationship
-
-def get_shortest_path_matrix(net):
-    """
-    calculate the shortest path length between all pairs of nodes in the network.
-    unconnected nodes are marked with inf.
-    :return: distance matrix of edges between nodes
-    """
-    n = nx.number_of_nodes(net)
-    distance_matrix = np.full((n, n), np.inf)
-    np.fill_diagonal(distance_matrix, 0)  # Distance to self is 0
-
-    # use dijkstra algorithm to calculate the shortest path length
-    for source, paths in nx.shortest_path_length(net):
-        for target, length in paths.items():
-            distance_matrix[source, target] = length
-
-    return distance_matrix
-
-
-def get_euclidean_matrix(net):
-    """
-        Calculate the Euclidean distance between all pairs of nodes in the network.
-        unconnected nodes are marked with inf.
-
-        :param net:
-        :return:
-        """
-    n = range(nx.number_of_nodes(net))
-    # Extract node positions into a numpy array
-    positions = nx.get_node_attributes(net, 'pos')
-    pos_array = np.array([positions[node] for node in sorted(net.nodes())])
-    # Calculate the Euclidean distance matrix using broadcasting
-    diff = pos_array[:, np.newaxis, :] - pos_array[np.newaxis, :, :]
-    distance_matrix = np.sqrt(np.sum(diff ** 2, axis=-1))
-
-    # If the network is connected, return the distance matrix
-    if not nx.is_connected(net):
-        pairs = list(combinations(n, 2))
-        # insert inf for each pair of nodes that are not connected
-        for u, v in pairs:
-            if not nx.has_path(net, source=u, target=v):
-                distance_matrix[u, v] = np.inf
-                distance_matrix[v, u] = np.inf
-
-    return distance_matrix
-
-
-
-def random_walk(net, start, end):
-    current_node = start
-    steps = 0
-    while current_node != end:
-        neighbors = list(net.neighbors(current_node))
-        current_node = random.choice(neighbors)
-        steps += 1
-    return steps
-
-def compute_random_walk_distance(net, u, v):
-    steps = [random_walk(net, u, v) for _ in range(50)]
-    return u, v, np.mean(steps)
-
-def get_random_walk_matrix(net, num_workers=None):
-    """
-    Calculate the random walk distance between all pairs of nodes in the network.
-    :param num_workers: Number of threads to use for parallel processing.
-    :return: Distance matrix of edges between nodes
-    """
-    n = range(nx.number_of_nodes(net))
-    distance_matrix = np.full((max(n) + 1, max(n) + 1), np.inf)
-    np.fill_diagonal(distance_matrix, 0)
-    pairs = list(combinations(n, 2))
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-        futures = [executor.submit(compute_random_walk_distance, net, u, v) for u, v in pairs if nx.has_path(net, u, v)]
-        for future in concurrent.futures.as_completed(futures):
-            u, v, res = future.result()
-            distance_matrix[u, v] = res
-            distance_matrix[v, u] = res
-
-    return distance_matrix
-
-def find_connected_components(net):
-    """
-    Find all connected components in the network.
-    use only compoents bigger than 2 nodes.
-    :param matrix: distance matrix.
-    :return: adjacency matrices of all components.
-    """
-    indices = []
-
-    components = list(nx.connected_components(net))
-    for comp in components:
-        comp_nodes = list(comp)
-        if len(comp_nodes) > 3:
-            indices.append(comp_nodes)
-    return indices
 
 
 def calculate_mantel(net, fst_matrix, dist_type, perms):
@@ -455,6 +62,27 @@ def calculate_mantel(net, fst_matrix, dist_type, perms):
     weighted_p = np.ma.average(masked_p_values, weights=weights)
 
     return weighted_r, weighted_p
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ########### analysis of fst-distance
