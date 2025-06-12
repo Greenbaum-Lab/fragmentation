edff1ce77fc5bbf8a3b40b6598cad3b2662a05b2
ohad-peled <ohad.peled@mail.huji.ac.il>
2025-06-12 12:24:36 +0300

    restrucre matel functions and refactor plotting

--- mantel_test.py
+++ mantel_test.py
@@ -1,16 +1,9 @@
-import networkx as nx
-import numpy as np
 from mantel import test
-
 from distance_matrices import get_euclidean_matrix, get_shortest_path_matrix, get_random_walk_matrix, \
     find_connected_components
-from funcs import load_data
-
 import numpy as np
 import networkx as nx
-from scipy.stats import pearsonr
 from typing import Optional
-
 import concurrent.futures
 import pandas as pd
 
@@ -36,9 +29,7 @@ def calculate_mantel(net, fst_matrix, dist_type: str, perms: int) -> Optional[tu
         raise ValueError(f"Unknown distance type: {dist_type}")
 
     if nx.is_connected(net):
-        # Mantel test for connected network
         r, p, _ = test(X=distance_matrix, Y=fst_matrix, perms=perms, method='pearson', ignore_nans=True)
-
         return r, p
 
     # Handle disconnected networks by computing Mantel for each component
@@ -64,7 +55,7 @@ def calculate_mantel(net, fst_matrix, dist_type: str, perms: int) -> Optional[tu
     return weighted_r, weighted_p
 
 
-def process_step(step, net, fst, dist_type, perms, replica):
+def mantel_step(step, net, fst, dist_type, perms, replica):
     """
     Process a single step for Mantel test.
     :param step: Step index
@@ -97,7 +88,7 @@ def calculate_mantel_process(data, perms, dist_type, replica, num_workers=None):
     fst_matrices = data.fst_matrices[replica]
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-        futures = [executor.submit(process_step, step, net, fst, dist_type, perms, replica)
+        futures = [executor.submit(mantel_step, step, net, fst, dist_type, perms, replica)
                    for step, (net, fst) in enumerate(zip(networks, fst_matrices))]
 
         for future in concurrent.futures.as_completed(futures):
@@ -144,92 +135,5 @@ def calculate_mantel_all(data, perms, dist_type: str):
 
     cor_data = pd.concat(results)
 
-    # Save results for each fragmentation type
     cor_data.to_csv(f'./csv_new/fst_{dist_type}_corrrlation.csv', index=False)
     return cor_data
-
-
-### calculate mantel for all for fragmentation types
-fragmentation_types = ['rand', 'cor', 'intr', 'reg', 'dist', 'div', 'opt', 'wrst']
-fragmentation_types = ['rand']
-data = load_data(fragmentation_types)
-perms = 999
-cor_data = calculate_mantel_all(data, perms, dist_type='euclidean')
-
-
-def plot_cor_fst(df):
-    """
-    plot mantel correlation for a single type or replica.
-    mini test.
-    """
-    # df = normalize_steps(df)
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(x='step', y='r_val', data=df)
-    plt.xlabel('Fragmentation (%)')
-    plt.ylabel('Correlation')
-    plt.show()
-
-def plot_mantel_all(df):
-    """
-    plot mantel correlation for all fragmentation types.
-    """
-    df['step'] = df['step'] / df['step'].max() * 100  # Normalize steps to percentage
-    df = df[df['p_val'] < 0.05]  # Filter rows with pval < 0.05
-    # Filter steps with at least 5 unique replicas
-
-    df = (
-        df
-        .groupby(['fragmentation_type', 'step'])
-        .filter(lambda g: g['replica'].nunique() >= 5)
-    )
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(x='step', y='r_val', hue='fragmentation_type', data=df, errorbar='sd', legend=False)
-    plt.xlabel('% fragmentation', fontsize=28)
-    plt.ylabel('Correlation (r)', fontsize=28)
-    plt.tick_params(axis='both', labelsize=25)
-    plt.ylim(-0.05, 1.1)
-    plt.savefig(f'./figs/cor_fst_euclidean_pval.svg', format="svg")
-    plt.show()
-
-
-
-#### plot single correlation fst-distance
-# fragmentation_types = ['wrst']
-# data = load_data(fragmentation_types)
-# data = data[fragmentation_types[0]]
-#
-# steps = [0, 75, 150]
-# fig, axes = plt.subplots(1, 3, figsize=(18, 7), sharey=True)
-#
-# for i, step in enumerate(steps):
-#     net = data[1][5][step]
-#     fst = data[7][5][step]
-#     distance_matrix = get_random_walk_matrix(net)
-#     r, p = calculate_mantel(net=net, fst_matrix=fst, dist_type='random', perms=999)
-#     print(r, p)
-#
-#     flat_matrix1 = distance_matrix.flatten()
-#     flat_matrix2 = fst.flatten()
-#     flat_matrix1 = flat_matrix1[flat_matrix1 != 0]
-#     flat_matrix2 = flat_matrix2[flat_matrix2 != 0]
-#     df = pd.DataFrame({'distance': flat_matrix1, 'fst': flat_matrix2})
-#     df = df.dropna()
-#     # filter inf
-#     df = df[~df['distance'].isin([np.inf, -np.inf])]
-#     print(df)
-#
-#     sns.regplot(x='distance', y='fst', data=df, fit_reg=True, order=1, ax=axes[i])
-#     axes[i].set_xlabel('Distance', fontsize=30)
-#     axes[i].set_ylabel(r'Pairwise $F_{ST}$' if i == 0 else '', fontsize=30)
-#     axes[i].tick_params(axis='both', labelsize=25)
-#     axes[i].set_ylim(0, 0.5)
-#     axes[i].text(0.05, 1.2, f'r={r:.2f}\np={p:.2e}', fontsize=20, transform=axes[i].transAxes)
-#
-# plt.tight_layout()
-# plt.savefig(f'./figs/random_fst_steps.svg', format="svg")
-# plt.show()
-
-# df = pd.read_csv('./TEST-corl_fst_euclidean_all.csv')
-# print(df)
-# plot_mantel_all(df)
-
