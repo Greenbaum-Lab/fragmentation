b4f05dc297adeb87b002f84ee4c46023e68a18a1
ohad-peled <ohad.peled@mail.huji.ac.il>
2025-06-26 14:13:06 +0300

    plot_genetics styling

--- scripts/plot_genetics.py
+++ scripts/plot_genetics.py
@@ -1,32 +1,43 @@
 import os
 
-from data_manipulation.manp_genetics import filter_intervals, select_random_nodes, extract_nodes, process_variance
-from funcs import  load_data
-from viz_funcs.viz_genetics import plot_genetics, plot_distribution, plot_het_nodes, plot_variance
+from distributions import filter_intervals, plot_distribution
+from funcs import load_data
+from mean_genetics import plot_genetics
+from pop_ind import select_random_nodes, extract_nodes, plot_het_nodes
 
-######################
-###################### plot data
-print(os.getcwd())
+def plot_genetic_data():
+    """
+    Plot genetic data for different fragmentation types.
+    """
+    fragmentation_types = ['rand']
+    data = load_data(fragmentation_types)
+    plot_genetics(data, measure='het')
 
-fragmentation_types = ['rand', 'cor', 'intr', 'reg', 'dist', 'div', 'opt','wrst']
-fragmentation_types = ['rand']
-data = load_data(fragmentation_types)
-plot_genetics(data, measure='het')
+def plot_distributions():
+    """
+    Plot distributions for a single fragmentation type.
+    """
+    fragmentation_types = ['rand']
+    data = load_data(fragmentation_types)
+    df = filter_intervals(data['rand'], measure='fst', interval_pct=25)
+    plot_distribution(df, measure='fst', frag_type='rand')
 
-#############plot distributions
-#### one frag type each time
-# fragmentation_types = ['rand']
-# data = load_data(fragmentation_types)
-# df = filter_intervals(data['rand'], measure='fst', interval_pct=25)
-# plot_distribution(df,measure='fst', frag_type='rand')
-#
-# ###################### plot individual nodes
-# ###############
-# fragmentation_types = ['rand', 'cor', 'intr', 'dist', 'reg', 'div', 'opt', 'wrst']
-# fragmentation_types = ['rand']
-# data = load_data(fragmentation_types)
-# df = data.get('rand').het_dist
-# x=select_random_nodes(df, per_replica=1)
-# df_selected = extract_nodes(df, x)
-# plot_het_nodes(df_selected, n_nodes=10)
-#
+def plot_individual_nodes():
+    """
+    Plot individual nodes for a single fragmentation type.
+    """
+    fragmentation_types = ['rand']
+    data = load_data(fragmentation_types)
+    df = data.get('rand').het_dist
+    selected_nodes = select_random_nodes(df, per_replica=1)
+    df_selected = extract_nodes(df, selected_nodes)
+    plot_het_nodes(df_selected, n_nodes=10)
+
+def main():
+    print(f"Current working directory: {os.getcwd()}")
+    plot_genetic_data()
+    plot_distributions()
+    plot_individual_nodes()
+
+if __name__ == "__main__":
+    main()
