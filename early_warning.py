b1e0684063972c31d61f39b7da6ad236e1e00d2b
ohad-peled <ohad.peled@mail.huji.ac.il>
2025-06-26 14:23:30 +0300

    reorganize early warning script, delete old functions

--- early_warning.py
+++ early_warning.py
@@ -1,38 +1,9 @@
-import os
-import pickle
 import networkx as nx
-import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from statsmodels.tsa.ar_model import AutoReg
 import random
-from scipy.stats import skew, kurtosis
 
-# from funcs import access_het_dist, access_networks, normalize_steps
-
-
-#show the first 1000 rows
-# pd.set_option('display.max_rows', None)
-
-##################to remove this func
-# def assign_node_numbers(df, nodes_per_step=50):
-#     """
-#     Assigns node numbers for each node in each replica.
-#
-#     :param df: DataFrame containing the heterozygosity data (dat[2]).
-#     :param nodes_per_step: Number of nodes in each step. i.e. the number of nodes in the network.
-#     :return: DataFrame with the original data and an additional 'node_number' column.
-#     """
-#     dfs = []  # List to collect DataFrames
-#     df = access_het_dist(df)
-#     for replica in df['replica'].unique():
-#         df_replica = df[df['replica'] == replica]
-#         for index in range(nodes_per_step):
-#             node_rows = df_replica.iloc[index::nodes_per_step].copy()
-#             node_rows['node_number'] = index  # Assign node number
-#             dfs.append(node_rows)
-#     df_with_node_numbers = pd.concat(dfs, ignore_index=True)  # Concatenate all DataFrames at once
-#     return df_with_node_numbers
+from funcs import assign_node_numbers
 
 
 def get_focal_step(df, num_nodes=10):
@@ -109,25 +80,6 @@ def export_het_csv(data, frag: str):
     return final_df
 
 
-####################################################
-######################### analysis #################
-
-######## read the pickle file with RGG (d-0.6) data
-
-# with open(f'cor_d0.6_r1000.pickle', 'rb') as file:
-#     cor = pickle.load(file)
-# print('finish')
-# frag = 'cor_d0.6_r1000'
-#save the heterozygosity data to a csv file
-# export_het_csv(cor, frag)
-
-
-#plot nodes
-# cor= pd.read_csv('cor_d0.6_r1000_het.csv')
-# print(cor)
-# plot_random_nodes(cor, num_nodes=10)
-
-# follow the largest component in the network for each replica across the fragmentation procss
 def get_largest_component(nets):
     """
     get the largest component of each network in each replica and step.
@@ -147,79 +99,6 @@ def get_largest_component(nets):
     return pd.DataFrame(components)
 
 
-# load the data
-# nets = access_networks(cor)
-# het = assign_node_numbers(cor)
-# components = get_largest_component(nets)
-#
-# # get the heterozygosity data for the corresponding largest component
-# component_data = pd.merge(het, components, on=['replica', 'step', 'node_number'])
-# component_data = component_data.sort_values(by=['replica', 'step', 'node_number'])
-# component_data = component_data[component_data['replica'].between(0, 2)]
-
-# plot the heterozygosity data of all nodes of replica 0 with step on x axis and het as y axis
-# df = component_data[(component_data['replica'] == 0) & (component_data['node_number'] == 43)]
-# # print(df)
-# plt.plot(df['step'], df['het'])
-# plt.show()
-
-# steps_to_plot = [0, 300, 600, 700]
-# for step in steps_to_plot:
-#     df = component_data[(component_data['replica'] == 0) & (component_data['step'] == step)]
-#     plt.figure()
-#     plt.hist(df['het'], bins=15, alpha=0.7)
-#     plt.title(f'Histogram of het at step {step}')
-#     plt.xlabel('het')
-#     plt.ylabel('Frequency')
-#     plt.show()
-####### calculate the sd skewness and kurtosis for the heterozygosity data
-
-
-import warnings
-warnings.filterwarnings('ignore', category=Warning, module='statsmodels')
-
-from statsmodels.tsa.ar_model import AutoReg
-
-
-def calculate_return_rate(window_data):
-    """
-    Calculate return rate from the AR(1) coefficient.
-    """
-    # Fit AR(1) model
-    model = AutoReg(window_data, lags=1, trend='n').fit()
-    ar1 = model.params[0]  # AR(1) coefficient
-
-    return 1 / ar1  # Return rate is the inverse of the AR(1) coefficient
-
-
-def calculate_return_rate_df(df):
-    """
-    Process the DataFrame to calculate AR(1) and return rate for each step and replica.
-    """
-    results = []
-
-    for replica, group in df.groupby('replica'):
-        for step in group['step'].unique():
-            window_data = group[group['step'] == step]['het'].values
-            # Ensure there are enough data points for AR(1)
-            if len(window_data) < 3:
-                continue  # Skip if less than 3 data points
-            # Calculate return rate
-            return_rate = calculate_return_rate(window_data)
-            results.append({'replica': replica, 'step': step, 'returnrate': return_rate})
-
-    # Create a DataFrame with results
-    results_df = pd.DataFrame(results)
-    return results_df
-
-
-# cor = pd.read_csv('cor_d0.6_r1000_component.csv')
-#
-# rr = process_df(cor)
-# print(rr)
-# plt.plot(rr['step'], rr['returnrate'])
-# plt.show()
-
 
 def calculate_indicators(data):
     """
@@ -233,26 +112,7 @@ def calculate_indicators(data):
     indicators['kurt'] = grouped.apply(pd.Series.kurtosis).values
     indicators.to_csv(f'indicators.csv', index=False)
 
-    # Calculate return rates
-    rr_df = calculate_return_rate_df(data)
-    print(rr_df)
-    indicators = pd.merge(indicators, rr_df, on=['step', 'replica'], how='left')
-
     return indicators
-
-# cor = pd.read_csv('cor_d0.6_r1000_component_truncated.csv')
-# indicators = calculate_indicators(cor)
-# indicators.to_csv('indicators_metapop_truncated.csv', index=False)
-
-# Truncate the DataFrame at step 650
-# indicators = pd.read_csv('indicators_singlepop.csv')
-# truncated_indicators = indicators[indicators['step'] < 650]
-# # Save the truncated DataFrame to a new CSV file
-# truncated_indicators.to_csv('truncated_indicators.csv', index=False)
-# print("Truncated DataFrame saved as 'truncated_indicators.csv'")
-
-
-#### final plot
 
 
 def plot_het_indicator(cor: pd.DataFrame,
@@ -355,109 +215,3 @@ def plot_het_indicator(cor: pd.DataFrame,
 
     plt.savefig(f'./figs/het_{indicator}_signelpop.svg', format='svg')
     plt.show()
-
-
-# plot het+indicators of metapopilation data-change y label
-# set random seed for reproducibility
-# random.seed(1)
-# cor = pd.read_csv('cor_d0.6_r1000_het.csv')
-# indicators = pd.read_csv('indicators_metapop.csv')
-# plot_het_indicator(cor, indicators, indicator='kurt', n_samples=10)
-
-# plot het+indicators of single population data-change y label
-# set random seed for reproducibility
-
-random.seed(1)
-cor = pd.read_csv('cor_d0.6_r1000_het.csv')
-indicators = pd.read_csv('indicators_singlepop_25.csv')
-plot_het_indicator(cor, indicators, indicator='kurt', n_samples=10)
-
-
-#### get the heterozygosity data for the corresponding largest component into csv
-# with open(f'cor_d0.6_r1000.pickle', 'rb') as file:
-#     cor = pickle.load(file)
-# print('finish')
-# nets = access_networks(cor)
-# het = assign_node_numbers(cor)
-# print(het)
-# components = get_largest_component(nets)
-# print(components)
-# # get the heterozygosity data for the corresponding largest component
-# component_data = pd.merge(het, components, on=['replica', 'step', 'node_number'])
-# component_data = component_data.sort_values(by=['replica', 'step', 'node_number'])
-# component_data.reset_index().to_csv('cor_d0.6_r1000_full.csv', index=False)
-
-# print(component_data)
-# mean_het_per_step_replica = component_data.groupby(['replica', 'step'])['het'].mean()
-# pd.set_option('display.float_format', '{:.10f}'.format)
-#
-# print(mean_het_per_step_replica)
-# # Save the resulting DataFrame to a CSV file
-# mean_het_per_step_replica.reset_index().to_csv('cor_d0.6_r1000_mean_het.csv', index=False)
-# print(mean_het_per_step_replica.reset_index())
-
-######## truncate data before tipping point
-# cor = pd.read_csv('cor_d0.6_r1000_full.csv')
-# results = []
-# for replica in cor['replica'].unique():
-#     # Get the data for the current replica
-#     replica_data = cor[cor['replica'] == replica]
-#     # Loop over each unique node_number for the current replica
-#     for node in replica_data['node_number'].unique():
-#         # Get the data for the current node in the replica
-#         node_data = replica_data[replica_data['node_number'] == node]
-#         # Extract step and het values
-#         time = node_data['step']
-#         het = node_data['het']
-#         # Compute the first derivative (slope) and second derivative (acceleration)
-#         slope = np.gradient(het, time)
-#         acceleration = np.gradient(slope, time)
-#         # Find the step where acceleration is minimum
-#         min_accel_index = np.argmin(acceleration)
-#         min_accel_step = time.iloc[min_accel_index]  # Get the corresponding step
-#         # Store the result in the results list
-#         results.append({
-#             'replica': replica,
-#             'node_number': node,
-#             'min_acceleration_step': min_accel_step
-#         })
-
-# results_df = pd.DataFrame(results)
-# cor = pd.merge(cor, results_df, on=['replica', 'node_number'], how='left')
-# cor_truncated = cor[cor['step'] < cor['min_acceleration_step']]
-# cor_truncated = cor_truncated.drop(columns=['min_acceleration_step'])
-# cor_truncated.to_csv('test2.csv', index=False)
-
-
-
-
-########## plot distribution of indicators of single pop
-# indicators = pd.read_csv('TESTkendall_metapop_truncated.csv')
-# print(indicators)
-# indicator_columns = ['kurt.tau', 'kurt.p', 'sk.tau','sk.p', 'sd.tau', 'sd.p', 'returnrate.tau', 'returnrate.p']
-#
-# # Create a figure with 2 columns and 4 rows
-# fig, axes = plt.subplots(4, 2, figsize=(15, 20))
-# axes = axes.flatten()
-#
-# # Iterate over each indicator column and plot
-# for i, col in enumerate(indicator_columns):
-#     ax = axes[i]
-#     median_value = indicators[col].median()
-#     ax.axvline(median_value, color='red', linestyle='dashed', linewidth=1.5)
-#     # Set x-axis limits based on the index
-#     if i in [0, 2, 4, 6]:  # i = 1, 3, 5, 7 (0-based indexing)
-#         ax.set_xlim(-1, 1)
-#         ax.hist(indicators[col], bins=np.linspace(-1, 1, 31), color='skyblue', edgecolor='black', alpha=0.7)
-#
-#     elif i in [1, 3, 5, 7]:  # i = 2, 4, 6, 8 (0-based indexing)
-#         ax.set_xlim(0, 0.5)
-#         ax.hist(indicators[col], bins=np.linspace(0, 0.5, 31), color='skyblue', edgecolor='black', alpha=0.7)
-#     ax.set_ylabel('Frequency', fontsize=30)
-#     ax.set_title(f'{col}', fontsize=30)
-#     ax.tick_params(axis='both', labelsize=24)
-#
-# # Adjust layout and show the plots
-# plt.tight_layout()
-# # plt.savefig('./figs/kendall_metapop.svg', format='svg')
-# plt.show()
