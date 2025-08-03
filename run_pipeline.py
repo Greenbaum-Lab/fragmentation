3d450049484a2cac5fd48d835b6bbeb72bdd582e
ohad-peled <ohad.peled@mail.huji.ac.il>
2025-08-03 14:52:55 +0300

    make compatible with refactored functions

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,6 +1,6 @@
 import pickle
-import pandas as pd
-from funcs_initial_data import make_networks, make_replicates_new, make_rgg
+from funcs_initial_data import run_replicates
+from networks_generator import make_rgg
 
 ######### run pipeline
 print("here i start!")
@@ -8,59 +8,60 @@ n_nodes = 50  # no. of nodes
 n_rep = 100
 n_edges = 250
 net = "RGG"
-ignore = False
 
 # # # create list off nets
-nets =  make_rgg(n_nets=n_rep, n_nodes=n_nodes, target_edges=n_edges)
+nets =  make_rgg(n_graphs=n_rep, n_nodes=n_nodes, target_edges=n_edges,asymmetric=True)
+print("nets created")
+
 
 # run the pipeline for all fragmentation types
-rand = make_replicates_new(nets=nets, frag_type='rand', ignore=ignore)
+rand = run_replicates(nets=nets, frag_key='rand',n_workers=20)
 print("1")
-pickle_filename = f'{net}, rand_ignore_{ignore}.pickle'
+pickle_filename = f'{net}, rand_asymmetric_TEST.pickle'
 with open(pickle_filename, 'wb') as file:
     pickle.dump(rand, file)
 del rand
 
-cor = make_replicates_new(nets=nets, frag_type='cor', ignore=ignore)
+cor = run_replicates(nets=nets, frag_key='cor')
 print("2")
-with open(pickle_filename, 'wb') as file:
+with open(f'{net}, cor_asymmetric_TEST.pickle', 'wb') as file:
     pickle.dump(cor, file)
 del cor
 
-intr = make_replicates_new(nets=nets, frag_type='intr', ignore=ignore)
+intr = run_replicates(nets=nets, frag_key='intr')
 print("3")
-with open(pickle_filename, 'wb') as file:
+with open(f'{net}, intr_asymmetric_TEST.pickle', 'wb') as file:
     pickle.dump(intr, file)
 del intr
 
-reg = make_replicates_new(nets=nets, frag_type='reg', ignore=ignore)
+reg = make_replicates_new(nets=nets, frag_key='reg')
 print("4")
-with open(pickle_filename, 'wb') as file:
+with open(f'{net}, reg_asymmetric_TEST.pickle', 'wb') as file:
     pickle.dump(reg, file)
 del reg
 
-div = make_replicates_new(nets=nets, frag_type='div', ignore=ignore)
+div = make_replicates_new(nets=nets, frag_key='div')
 print("5")
-with open(pickle_filename, 'wb') as file:
+with open(f'{net}, div_asymmetric_TEST.pickle', 'wb') as file:
     pickle.dump(div, file)
 del div
-
-dist = make_replicates_new(nets=nets, frag_type='dist', ignore=ignore)
+#
+dist = make_replicates_new(nets=nets, frag_key='dist')
 print("6")
-with open(pickle_filename, 'wb') as file:
+with open(f'{net}, dist_asymmetric_TEST.pickle', 'wb') as file:
     pickle.dump(dist, file)
 del dist
-
-opt = make_replicates_new(nets=nets, frag_type='opt', ignore=ignore)
+#
+opt = make_replicates_new(nets=nets, frag_key='opt')
 print("7")
-with open(pickle_filename, 'wb') as file:
+with open(f'{net}, opt_asymmetric_TEST.pickle', 'wb') as file:
     pickle.dump(opt, file)
 del opt
-
-# opt2 = make_replicates_new(nets=nets, frag_type='opt2', ignore=ignore)
-# print("8")
-# wrst = make_replicates_new(nets=nets, frag_type='wrst', ignore=ignore)
-# print("9")
-
+#
+wrst = make_replicates_new(nets=nets, frag_key='wrst')
+print("8")
+with open(f'{net}, wrst_asymmetric_TEST.pickle', 'wb') as file:
+    pickle.dump(wrst, file)
+del wrst
 print("finish")
 ########## finish pipeline
